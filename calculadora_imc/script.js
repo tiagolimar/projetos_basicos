93cb539d54dd549236efe6e0cb117b5ca706769d
tiagolimar <tiagolima.r2@gmail.com>
2023-01-25 22:52:37 -0300

    conclusão da calculadora imc

--- calculadora_imc/script.js
+++ calculadora_imc/script.js
@@ -1,44 +1,50 @@
-function checagem(){
-    let valores = document.getElementsByTagName("input")
+function Checagem(){
+    let valores = document.getElementsByTagName("input");
     let sem_valores_vazios = true;
 
-    Array.from(valores).forEach(function(element) {
+    Array.from(valores).forEach(function(element){
         if (element.value == ""){
-            alert('Escreva um valor para o campo '+element.name);
+            alert('Escreva um valor para o campo '+element.id);
             sem_valores_vazios = false;
         }
     });
     
     if (sem_valores_vazios){
-        calcular(valores);
+        Calcular(valores);
     }
 }
 
-function calcular(valores){
+function Calcular(valores){
+    let resultado = document.getElementById("resultado");
+    let imagens = document.getElementsByTagName("img");
+    let peso = Number(valores[0].value);
+    let altura = Number(valores[1].value);
 
-    let pessoa = {
-        nome : valores[0].value,
-        altura : valores[1].value,
-        peso : valores[2].value,
-    };
-    
-    pessoa.imc = pessoa.peso/pessoa.altura**2;
+    imc = peso/(altura**2);
 
     let classificacao = new Map();
-   
+    classificacao.set(18.5,"Peso baixo");
     classificacao.set(24.9,"Peso normal");
     classificacao.set(29.9,"Sobrepeso");
     classificacao.set(34.9,"Obesidade grau I");
     classificacao.set(40,"Obesidade grau II");
     classificacao.set(2000,"Obesidade grau III");
 
+    let i = 0;
+
     for(let valor of classificacao.keys()) {
-        if (pessoa.imc<=valor){
-            pessoa.imc_tipo = classificacao.get(valor);
+        if (imc<=valor){
+            RemoverVisibilidade(imagens);
+            imagens[i].classList.add("visivel");
+            resultado.innerHTML = 'Você é uma pessoa com ' + classificacao.get(valor);
             break;
         }
+        i++;
     }
-    
-    texto_saida = pessoa.nome + ' tem ' + pessoa.imc_tipo;
-    document.getElementsByTagName("textarea")[0].value = texto_saida;
+}
+
+function RemoverVisibilidade(imagens){
+    Array.from(imagens).forEach(function(imagem){
+        imagem.classList.remove("visivel")
+    });
 }
