fec7cd1be7aeb1da3314fd5cfab5e2595a61a062
tiagolimar <tiagolima.r2@gmail.com>
2023-02-02 12:58:27 -0300

    correção da rotação dos ponteiros do relógio

--- relógio/script.js
+++ relógio/script.js
@@ -4,13 +4,20 @@ let ponteiro_segundo = document.getElementById("segundo");
 
 const relogio = setInterval(function time(){
     let dateToday = new Date();
-    let hr = dateToday.getHours();
-    let contador_minuto = dateToday.getMinutes();
-    let contador_segundo = dateToday.getSeconds();
+    let hr = Number(dateToday.getHours());
+    let contador_minuto = Number(dateToday.getMinutes())*6;
+    let contador_segundo = Number(dateToday.getSeconds())*6;
 
-    let contador_hora = hr*5;
+    if(hr < 13){
+        contador_hora = hr*30;
+    }else{
+        contador_hora = (hr-12)*30;
+    }
 
-    ponteiro_hora.style("transform:rotate("+contador_hora+"deg)");
-    ponteiro_minuto.style("transform:rotate("+contador_minuto+"deg)");
-    ponteiro_segundo.style("transform:rotate("+contador_segundo+"deg)");
+    contador_minuto = contador_minuto + (contador_segundo/60);
+    contador_hora = contador_hora + 6*(contador_minuto/60);
+
+    ponteiro_hora.style ="transform:rotate("+contador_hora+"deg)";
+    ponteiro_minuto.style ="transform:rotate("+contador_minuto+"deg)";
+    ponteiro_segundo.style ="transform:rotate("+contador_segundo+"deg)";
 },1000)
