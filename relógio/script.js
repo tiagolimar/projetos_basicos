101c8ada8702c78f8da625ce778edbc9d65d0009
tiagolimar <tiagolima.r2@gmail.com>
2023-02-04 21:39:00 -0300

    organização de pastas e melhora no estilo do relógio

--- relógio/script.js
+++ relógio/script.js
@@ -2,8 +2,13 @@ let ponteiro_hora = document.getElementById("hora");
 let ponteiro_minuto = document.getElementById("minuto");
 let ponteiro_segundo = document.getElementById("segundo");
 
+const medidor_hora = document.getElementById("medidor_hora");
+const medidor_minuto = document.getElementById("medidor_minuto");
+const quant_medidor_minuto = 360/60;
 
-const relogio = setInterval(function time(){
+setInterval(time,1000);
+
+function time(){
     let dateToday = new Date();
     let hr = Number(dateToday.getHours());
     let contador_minuto = Number(dateToday.getMinutes())*6;
@@ -15,10 +20,36 @@ const relogio = setInterval(function time(){
         contador_hora = (hr-12)*30;
     }
 
-    contador_minuto = contador_minuto + (contador_segundo/60);
-    contador_hora = contador_hora + 6*(contador_minuto/60);
+    let ajuste = 0;
 
+    contador_minuto = contador_minuto + (contador_segundo/60);
+    contador_hora = contador_hora + 6*((contador_minuto/60)-ajuste);
+    
     ponteiro_hora.style ="transform:rotate("+contador_hora+"deg)";
     ponteiro_minuto.style ="transform:rotate("+contador_minuto+"deg)";
     ponteiro_segundo.style ="transform:rotate("+contador_segundo+"deg)";
-},1000)
+    
+}
+
+function CriarMedidores(){
+    for (let i = 0;i < 11;i++){
+        copia = medidor_hora.cloneNode(true);
+        if (i<9){
+            copia.innerHTML = " " + (i+1);
+        }else{
+            copia.innerHTML = i+1;
+        }
+        copia.style = "transform: rotate("+((i+1)*30)+"deg)";
+        medidor_hora.parentNode.appendChild(copia);
+    }
+
+    for (let i = 2;i < 60;i++){
+        if(i%5!=0){
+            copia = medidor_minuto.cloneNode(true);
+            copia.style = "transform: rotate("+i*6+"deg)";
+            medidor_minuto.parentNode.appendChild(copia);
+        }
+    }
+}
+
+CriarMedidores();
--- testes/script.js
+++
@@ -1,7 +0,0 @@
-corpo = document.getElementsByTagName("body")[0];
-caixa = document.getElementsByClassName("box")[0];
-
-let letra = document.createElement('div');
-letra.setAttribute("class","letra");
-
-caixa.insertBefore(letra,corpo);
