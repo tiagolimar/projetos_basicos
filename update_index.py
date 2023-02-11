4aea414ba274948912b4f508bd2eab4ab269b081
tiagolimar <tiagolima.r2@gmail.com>
2023-02-11 09:52:06 -0300

    adicionando data de modificação no index.html principal

--- update_index.py
+++ update_index.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 
 nome_html = 'index.html'
 excludente_de_pastas = ['_','.']
@@ -8,6 +8,13 @@ local_html_base = os.getcwd()+'\\'+'_annotate\\index_base.html'
 identificador_conteudo = '<<<>>>'
 novo_conteudo = ''
 texto_html = ''
+
+def obter_data_modificacao(nome_da_pasta):
+    info_pasta = os.stat(pasta).st_mtime
+    data = datetime.fromtimestamp(info_pasta)
+    data_de_modificacao = datetime.strftime(data,"%d/%m/%Y %H:%M")
+    return data_de_modificacao
+
 
 def identificar_pastas():
     caminho_atual = os.getcwd()
@@ -26,14 +33,21 @@ def identificar_pastas():
             if incluir_na_lista:
                 diretorios.append(diretorio)
 
-
     return diretorios
+
 
 pastas = identificar_pastas()
 
-for pasta in pastas:
+for num,pasta in enumerate(pastas):
+    data_de_modificacao = obter_data_modificacao(pasta)
+
     nome_pasta = pasta.upper().replace('_',' ')
-    texto_html += f'\t\t<a target="_blank" href="{pasta}\\{nome_html}">{nome_pasta}</a>\n'
+    texto_html += f'''\t\t<div class="projeto link{num}" >
+            <a target="_blank" id="link{num}" href="{pasta}\\{nome_html}">{nome_pasta}</a>
+            <p>modificado em: {data_de_modificacao}</p>
+        </div>\n'''
+else:
+    texto_html = texto_html[:-1]
 
 with open(local_html_base,'r',encoding='utf-8') as arquivo:
     conteudo = arquivo.readlines()
