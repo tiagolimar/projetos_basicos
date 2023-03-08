de52399685fb9fe659bbf26e5daa3b9cee882dc5
tiagolimar <tiagolima.r2@gmail.com>
2023-03-07 23:14:11 -0300

    desenvolvendo animações posições e rotações aleatórias

--- 8. star_wars/script.js
+++ 8. star_wars/script.js
@@ -1,72 +1,36 @@
-let star = document.getElementById("star");
-let new_star = star;
-
-function choice_coord() {
-  let cy = Math.random() * 100;
-  let cx = Math.random() * 100;
-  let style_c = "top:"+cy+"%;left:"+cx+"%;"
-  return style_c;
-}
-
-function find_dist(){
-  let y = innerHeight;
-  let x = innerWidth;
-  if (y>x){return y/2}else{return x/2}
-}
-
 function main() {
-  let angle = Math.random()*360;
-  let rad = angle * Math.PI / 180;
-  let scale = 100;
-  let dist_max = find_dist();
-  let x = Math.cos(rad)*dist_max;
-  let y = Math.sin(rad)*dist_max;
+  let little_star = document.createElement('div');
+  let root = document.querySelector(':root');
+
+  little_star.id = 'star';
+
+  function setPosition(){
+    y = Math.random()*100;
+    x = Math.random()*100;
   
-  const sheet = new CSSStyleSheet();
-  const keyframes = new KeyframeEffect(
-    star,
-    [
-      { transform: "translate(0px, 0px) rotate("+rad+"rad) scale(1,1)" },
-      { transform: "translate("+x+"px, "+y+"px) rotate("+rad+"rad) scale("+scale+",1)" }
-    ],
-    { duration: 1000 }
-  );
-  
-  // const styleSheet = document.styleSheets[1];
-  console.log(styleSheet);
-  sheet.insertRule("@keyframes hyperspace {"+keyframes.cssText+"}");
-  document.adoptedStyleSheets = [sheet];
+    little_star.style.top = y+'%';
+    little_star.style.left = x+'%';
+  }
 
-//   function animate() {
-//     dist += 20;
-//     scale += 2;
+  function setRotation(){
+    dy = 50-y;
+    dx = x-50;
+    tan = dy/dx;
+    ang = (Math.atan(tan)*(180/Math.PI)+90)*-1;
+    root.style.setProperty('--ang0',ang+'deg');
+    console.log(x.toFixed(2) +" ; "+y.toFixed(2)+" ; "+dx.toFixed(2)+" ; "+dy.toFixed(2)+" ; "+ang.toFixed(2));
+  }
 
-//     x = Math.cos(rad)*dist;
-//     y = Math.sin(rad)*dist;
-    
-//     const transform = "translate(${x}px, ${y}px) rotate(${rad}rad) scale(${scale},1)";
-    
-//     star.style.transform = transform;
-    
-//     window.requestAnimationFrame(animate);
-    
-//     if (x > window.innerWidth/2 || y > window.innerHeight/2){
-//       star.remove();
-//       star = document.createElement("div");
-//       star.id = "star";
-//       document.body.appendChild(star);
-//       main();
-//     }
-//   }
+  setPosition();
+  setRotation();
 
-//   animate();
-}
+  document.body.appendChild(little_star);
 
-function duplicate_star() {
-  star_copy = star.cloneNode(star);
-  styleC = choice_coord();
-  star_copy.style = styleC;
-  star.parentNode.appendChild(star_copy);
+  little_star.addEventListener('animationend', () => {
+    little_star.remove();
+    main();
+  });
+
 }
 
 main();
