e726e688f9fb06d08301a57bb8800eeef3bcdc9f
tiagolimar <tiagolima.r2@gmail.com>
2023-03-12 14:30:35 -0300

    conclusão da primeira fase da animação de hiperespaço

--- 8. star_wars/script.js
+++ 8. star_wars/script.js
@@ -1,26 +1,22 @@
+let root = document.querySelector(':root');
+// let vel_animation = root.style.getProperty('--vel');
+// console.log(vel_animation);
 function main() {
-  let cateto_o = document.getElementById('cateto-o');
-  let cateto_a = document.getElementById('cateto-a');
-
-
   let little_star = document.createElement('div');
-  let root = document.querySelector(':root');
-  let display = document.querySelector('.display-angle');
 
   let originX = window.innerWidth/2;
   let originY = window.innerHeight/2;
   let y = Math.random()*window.innerHeight*0.10;
   let x = Math.random()*window.innerWidth*0.20;
 
-  little_star.id = 'star';
+  little_star.classList.add('star');
+  little_star.classList.add(`i-${contador}`);
 
   function setPosition(){
     y = [-y, y][Math.floor(Math.random()*2)]+originY;
     x = [-x, x][Math.floor(Math.random()*2)]+originX;
 
     little_star.style = `top:${y}px;left:${x}px`;
-    cateto_o.style = `top:${y}px;left:${x}px`;
-    cateto_a.style = `top:${y}px;left:${x}px`;
   }
 
   function setRotation(){
@@ -44,22 +40,7 @@ function main() {
       ang = 270 - ang;
     }
 
-    root.style.setProperty('--ang0',ang+'deg');
-
-    cateto_a.style.height = adx + 'px';
-    cateto_o.style.height = ady + 'px';
-    if (dy < 0){
-      cateto_o.style.transform = 'rotate(180deg)';
-    }else{
-      cateto_o.style.transform = 'rotate(0deg)';
-    }
-    if (dx < 0){
-      cateto_a.style.transform = 'rotate(270deg)';
-    }else{
-      cateto_a.style.transform = 'rotate(90deg)';
-    }
-
-    display.innerHTML = `${dx.toFixed(2)}px - ${dy.toFixed(2)}px - ${ang.toFixed(2)}°`;
+    root.style.setProperty(`--ang${contador}`,ang+'deg');
   }
 
   setPosition();
@@ -71,7 +52,16 @@ function main() {
     little_star.remove();
     main();
   });
+
+  contador++;
+  if(contador == 180){
+    contador = 0;
+    clearInterval(constelation);
+  }
 }
 
-main();
+let contador = 0;
+
+constelation = setInterval(main, 700/180);
+
 
