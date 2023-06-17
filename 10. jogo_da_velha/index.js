28754f46cad8e29f9b4a60715901aff8aba1bfd5
tiagolimar <tiagolima.r2@gmail.com>
2023-06-17 02:55:39 -0300

    conclusão do jogo da velha

--- 10. jogo_da_velha/index.js
+++ 10. jogo_da_velha/index.js
@@ -3,9 +3,26 @@
 let fields = {}
 Array.from(document.querySelectorAll('.field')).forEach(f=>fields[f.id] = f)
 
+let restart = false
 let player = 0
 let fields_zero = []
 let fields_one = []
+
+let init = () => {
+    success.innerHTML = ''
+    danger.innerHTML = ''
+    comentary.innerHTML = 'Sua vez! Clique AQUI para reiniciar'
+    player = 0
+    fields_zero = []
+    fields_one = []
+    restart = false
+    fields = {}
+    Array.from(document.querySelectorAll('.field')).forEach(f=>{
+        f.innerHTML = ''
+        fields[f.id] = f
+    })
+    play()
+}
 
 let play = e=>{
     if(!player){
@@ -16,7 +33,7 @@ let play = e=>{
         fields[id].innerHTML = 'O'
         fields_zero.push(id)
         delete fields[id]
-        test_vitory(fields_zero)
+        test_vitory(fields_zero,0)
         player = 1
     }else{
         if(!e.target.innerHTML){
@@ -26,26 +43,42 @@ let play = e=>{
             fields_one.push(id)
             delete fields[id]
             
-            test_vitory(fields_one)
+            test_vitory(fields_one,1)
             player = 0
-            play()
+            if(!restart) play()
         }
     }
 }
 
-let test_vitory = (list)=>{
+let test_vitory = (list,status)=>{
     if (list.length>2){
         list = list.sort()
 
         if(list.length==3){
-            if(is_victory(list)) console.log('vistory');
+            if(is_victory(list)) after_victory(status)
         }else{
             getCombinations(list,3).forEach(combination=>{
-                if(is_victory(combination)) console.log('victory');
+                if(is_victory(combination)) after_victory(status)
             })
 
         }
+        if(Object.keys(fields)==0) after_victory(2)
     }
+}
+
+let after_victory = (status)=>{
+    if (!status){
+        danger.innerHTML = 'Você perdeu!'
+    }else if (status==1){
+        success.innerHTML = 'Você venceu!'
+    }else if(status==2){
+        console.log(1);
+        comentary.innerHTML = 'Empate!<br>Clique AQUI para reiniciar'
+        restart = true
+        return
+    }
+    comentary.innerHTML = 'Clique AQUI para reiniciar.'
+    restart = true
 }
 
 let is_victory = (list)=>{
@@ -53,10 +86,12 @@ let is_victory = (list)=>{
     let dif1 = list[1]-list[0]
     let dif2 = list[2]-list[1]
 
-    if(dif1 != 2 && dif2 != 2){
-        if((dif1 == 1 && dif2 == 1)&&(list[0]==1 || list[0]==4 || list[0]==7)){
+    if(dif1 == dif2){
+        if((dif1 == 1 )&& (list[0]==1 || list[0]==4 || list[0]==7)){
             return true
-        }if ((dif1 == dif2)&&(dif1>2)){
+        }else if (dif1>2){
+            return true
+        }else if ((dif1 == 2)&&(list[0]==3)){
             return true
         }
     }
