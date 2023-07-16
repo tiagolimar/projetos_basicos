7793643d63922a1cbe5db3b8b2d4e3ef11bf956e
tiagolimar <tiagolima.r2@gmail.com>
2023-07-16 14:11:08 -0300

    adicionando secao de seguidores

--- 13. git_api/script.js
+++ 13. git_api/script.js
@@ -1,65 +1,111 @@
-const user = document.querySelector('#user');
-const img = document.querySelector('img');
-const list_repos = document.querySelector('#list_html');
-const loading = document.querySelector('#loading');
-const followers_container = document.querySelector('#followers_container');
+const user = document.querySelector("#user");
+const img_container = document.querySelector("#img_container");
+const img = document.querySelector("img");
+const list_repos = document.querySelector("#list_html");
+const loading = document.querySelector("#loading");
+const followers_container = document.querySelector("#followers_container");
+const followers_list = document.querySelector("#followers_list");
+let username;
 
 let updateLoading = () => {
-    let text = loading.querySelector('h3')
-    if(text){
+    let text = loading.querySelector("h3");
+    if (text) {
         text.remove();
-    }else{
-        loading.innerHTML = '<h3 class="loading text-light text-center">CARREGANDO...</h3>';
+    } else {
+        loading.innerHTML =
+            '<h3 class="loading text-light text-center">CARREGANDO...</h3>';
     }
-}
+};
 
 async function getUserInfo(username) {
     try {
         updateLoading();
-        const response = await fetch(`https://api.github.com/users/${username}`);
+        const response = await fetch(
+            `https://api.github.com/users/${username}`
+        );
         const userData = await response.json();
-        
+
         const { avatar_url, followers_url } = userData;
-        
+
         const response_followers = await fetch(followers_url);
         const followers = await response_followers.json();
-        
-        const repoResponse = await fetch(`https://api.github.com/users/${username}/repos`);
+
+        const repoResponse = await fetch(
+            `https://api.github.com/users/${username}/repos`
+        );
         const repoData = await repoResponse.json();
 
-        const repositories = repoData.map(repo => ({
+        const repositories = repoData.map((repo) => ({
             name: repo.name,
             url: repo.html_url,
-            data: repo.created_at
+            data: repo.created_at,
         }));
 
         const userInfo = [avatar_url, followers, repositories];
         updateLoading();
         return userInfo;
     } catch (error) {
-        console.error('Error:', error.message);
+        console.error("Error:", error.message);
         return null;
     }
 }
 
-function putImage(url){
-    img.style.display = 'initial';
+function removeTitle(father){
+    child = father.querySelector(".reset");
+    try{
+        child.remove();
+    }
+    catch{
+        return null;
+    }
+}
+
+function createTitulo(text) {
+    const titulo = document.createElement("h4");
+    titulo.classList.add("text-center");
+    titulo.classList.add("reset");
+    titulo.innerHTML = text;
+    return titulo;
+}
+
+function putImage(url) {
+    removeTitle(img_container);
+    img_container.insertBefore(createTitulo(username), img_user);
+    img.style.display = "initial";
     img.src = url;
 }
 
-function putImageFollowers(followers){
-    followers.forEach(follower=>{
-        console.log(follower.login);
-    })
+function putImageFollowers(followers) {
+    removeTitle(followers_container);
+    followers_container.insertBefore(
+        createTitulo("Seguidores"),
+        followers_list
+    );
+    followers_list.innerHTML = "";
+    followers.forEach((follower) => {
+        const { login, avatar_url, html_url } = follower;
+
+        const item = document.createElement("li");
+        item.classList.add("list-inline-item");
+
+        const card = `<div class="card mt-4" style="width: 8rem;">
+        <img src="${avatar_url}" class="card-img-top" alt="imagem de perfil de ${login}">
+        <div class="card-body bg-primary">
+          <a class="card-text text-white" href="${html_url}" target="_blank">${login}</a>
+        </div>
+      </div>`;
+        item.innerHTML = card;
+        followers_list.appendChild(item);
+    });
 }
 
-function listRepos(repositorios){
-    list_repos.innerHTML = '';
-    for(let rep of repositorios) {
-        const item = document.createElement('li');
-        item.classList.add('list-inline-item');
-        
-        const {name,url,data} = rep;
+function listRepos(repositorios) {
+    list_repos.innerHTML = "";
+    for (let rep of repositorios) {
+        const item = document.createElement("li");
+        item.classList.add("list-inline-item");
+
+        const { name, url, data } = rep;
         const card = `<div class="card m-2 bg-secondary" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title text-light">${name.toUpperCase()}</h5>
@@ -72,19 +118,20 @@ function listRepos(repositorios){
     }
 }
 
-function formatData(data){
-    let [year,month,day] = data.split('-');
-    day = day.substring(0,2);
+function formatData(data) {
+    let [year, month, day] = data.split("-");
+    day = day.substring(0, 2);
     return `${day}/${month}/${year}`;
 }
 
-user.addEventListener('keyup',e=>{
-    if(e.keyCode == 13) main();
-})
+user.addEventListener("keyup", (e) => {
+    if (e.keyCode == 13) main();
+});
 
 async function main() {
-    const data = await getUserInfo(user.value);
-    const [url,followers,repositorios] = data;
+    username = user.value;
+    const data = await getUserInfo(username);
+    const [url, followers, repositorios] = data;
     putImage(url);
     putImageFollowers(followers);
     listRepos(repositorios);
